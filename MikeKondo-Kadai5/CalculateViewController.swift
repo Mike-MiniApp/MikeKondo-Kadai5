9a749cac6d477c324191d6301f09246c82f37758
mikekondo <foad5969@gmail.com>
2022-05-25 15:32:10 +0900

    あきおさんFB反映

--- MikeKondo-Kadai5/CalculateViewController.swift
+++ MikeKondo-Kadai5/CalculateViewController.swift
@@ -8,37 +8,34 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
-    private enum AlertMessageType: String {
-        case number1IsNil = "割られる数を入力してください"
-        case number2IsNil = "割る数を入力してください"
-        case number2IsZero = "割る数には0以外の数を入力してください"
-    }
+    static let number1IsNil = "割られる数を入力してください"
+    static let number2IsNil = "割る数を入力してください"
+    static let number2IsZero = "割る数には0以外の数を入力してください"
 
     @IBOutlet private weak var number1TextField: UITextField!
     @IBOutlet private weak var number2TextField: UITextField!
     @IBOutlet private weak var calculatedNumberLabel: UILabel!
-    private var calculatedNumber: Float = 0
 
     @IBAction private func tappedCalculateButton(_ sender: Any) {
         guard let number1: Float = Float(number1TextField.text ?? "") else {
-            alert(alertMessage: AlertMessageType.number1IsNil.rawValue)
+            alert(message: CalculateViewController.number1IsNil)
             return
         }
         guard let number2: Float = Float(number2TextField.text ?? "") else {
-            alert(alertMessage: AlertMessageType.number2IsNil.rawValue)
+            alert(message: CalculateViewController.number2IsNil)
             return
         }
-        if number2 == 0 {
-            alert(alertMessage: AlertMessageType.number2IsZero.rawValue)
+        guard number2 != 0 else {
+            alert(message: CalculateViewController.number2IsZero)
             return
         }
-        calculatedNumber = Float(number1 / number2)
+        let calculatedNumber = Float(number1 / number2)
         calculatedNumberLabel.text = String(calculatedNumber)
     }
 
-    private func alert(alertMessage: String) {
+    private func alert(message: String) {
         let title = "課題5"
-        let message = alertMessage
+        let message = message
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
